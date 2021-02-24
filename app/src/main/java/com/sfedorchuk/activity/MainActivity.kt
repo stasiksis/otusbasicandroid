09751481e3b0d7c39ead7a022ad50562f3c9bb03
stasiksis <Stason94>
2021-02-24 10:31:38 +0300

    Small refactor

--- app/src/main/java/com/sfedorchuk/activity/MainActivity.kt
+++ app/src/main/java/com/sfedorchuk/activity/MainActivity.kt
@@ -3,7 +3,6 @@ package com.sfedorchuk.activity
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -22,11 +21,9 @@ class MainActivity : AppCompatActivity(), MoviesAdapter.MoviesClickListener {
 
     companion object {
         var IS_FAVORITE_SCREEN: Boolean = false
-        const val EXTRA_DATA = "EXTRA_DATA"
         const val KEY_ITEMS: String = "KEY_ITEMS"
     }
 
-    private var detailsFragment: MovieDetailsFragment? = null
 
     private var items =
         arrayListOf(
